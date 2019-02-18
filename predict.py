5b8d29f09d289284fabe0e12e2b86c3bfad45c36
Perry Brandiezs <pbrandiezs@gmail.com>
2019-02-18 14:54:30 -0800

    Added code to check the image.

--- predict.py
+++ predict.py
@@ -81,3 +81,157 @@ model = load_checkpoint(checkpoint)
 model.eval()
 
 print(model)
+
+
+
+
+#Process the test image
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    
+    #open the image
+    im = Image.open(image)
+
+    #resize the image
+    im = im.resize((255,255))
+    
+    #crop the image
+    left = 16
+    right = 240
+    top = 16
+    bottom = 240
+    im = im.crop((left, top, right, bottom))
+    np_image = np.array(im)
+    
+    # display the image being worked on.
+    flower_num = image.split('/')[2]
+    plt.imshow(np_image)
+    plt.title(cat_to_name[flower_num])
+    
+    # scale to between 0 and 1
+    np_image = np_image / 255
+    
+    mean = np.array([0.485, 0.456, 0.406])
+    std_dev = np.array([0.229, 0.224, 0.225])
+    
+    np_image = (np_image - mean) / std_dev
+    
+    #transpose 
+    np_image = np_image.transpose((2, 0, 1))
+    
+    #print the shape to verify
+    # print(np_image.shape)
+    
+    #return the converted image, ready for PyTorch
+    return np_image
+    
+process_image(input_image)
+
+
+
+
+def imshow(image, ax=None, title=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+# test it out
+imshow(process_image("flowers/test/1/image_06743.jpg"))
+
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    
+    # TODO: Implement the code to predict the class from an image file
+    proc_image = process_image(image_path)
+    
+    img = torch.from_numpy(proc_image).type(torch.FloatTensor)
+    img = img.unsqueeze(0)
+    
+    # print(img.shape)
+    
+    probs = torch.exp(model.forward(img))
+    # print("probs is", probs)
+    #print("probs.topk is ", probs.topk(topk))
+    top_probs, top_labs = probs.topk(topk)
+    
+    # print("top_probs", top_probs)
+    #print("top_labs ", top_labs)
+    top_probs = top_probs.detach().numpy().tolist()[0]
+    top_labs = top_labs.detach().numpy().tolist()[0]
+    # print("top_probs detached is ", top_probs)
+    # print("top_labs detached is ", top_labs)
+    
+    #index to classes
+    name_to_cat = {val: key for key, val in
+                   cat_to_name.items()}
+    
+    top_labels = []
+    top_flowers = []
+    # print("top_labs", top_labs)
+    # print("cat_to_name", cat_to_name)
+    #print("name for 21 ", cat_to_name['21'])
+    # for lab in top_labs:
+    #    print("name_to_cat of lab ", lab, "is ", cat_to_name[str(lab + 1)])
+    top_labels = [cat_to_name[str(lab + 1)] for lab in top_labs]
+    # top_flowers = [cat_to_name[lab] for lab in top_labs]
+    top_classes = [str(lab + 1) for lab in top_labs]
+    # print("Classes: ", top_classes)
+    # return top_probs, top_labels, top_flowers
+    return top_probs, top_labels, top_classes
+
+#test
+
+image_path = "flowers/test/1/image_06743.jpg"
+probs, labels, classes = predict(image_path, model)
+print(probs)
+print(labels)
+print(classes)
+
+
+
+
+
+
+# TODO: Display an image along with the top 5 classes
+
+#open the image
+image = "flowers/test/1/image_06743.jpg"
+im = Image.open(image)
+
+# display the image being worked on.
+flower_num = image.split('/')[2]
+np_image = np.array(im)
+
+
+fig = plt.figure(figsize = (6,10))
+plt.subplot(2,1,1)
+plt.imshow(np_image)
+plt.title(cat_to_name[flower_num])
+
+#display the results
+# probabilities, labels, flowers = predict(image, model)
+probabilities, labels, classes = predict(image, model)
+plt.subplot(2,1,2)
+
+sns.barplot(x = probabilities, y = labels, color = sns.color_palette()[0])
+plt.show()
